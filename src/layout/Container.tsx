8b21609f0ac8d78eff815562a64525472aec4993
Shawn Catanzarite <me@shawncatz.com>
2024-04-01 06:16:34 +0000

    initial

---
+++ src/layout/Container.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import MUIContainer from "@mui/material/Container";
+
+import { WrapErrorBoundary } from "common";
+
+export type ContainerProps = {
+  children: React.ReactNode;
+};
+export const Container = ({ children }: ContainerProps) => {
+  return (
+    <MUIContainer sx={{ overflow: "hidden", pb: 1, pt: 1 }}>
+      <WrapErrorBoundary>{children}</WrapErrorBoundary>
+    </MUIContainer>
+  );
+};
